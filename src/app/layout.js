6c303ee871400c04d8eb009f6d1301a23c2a3506
Rabby-khan-04 <ajrabbyk72@gmail.com>
2026-04-28 15:25:57 +0600

    Removed deprecated package

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,5 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Cursor from "@/component/shared/Cursor";
 import BackToTop from "@/component/shared/BackToTop";
 
 const inter = Inter({ variable: "--font-inter", subsets: ["latin"] });
@@ -14,7 +13,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${inter.variable} h-full antialiased`}>
       <body className={`${inter.className} bg-accent`}>
-        <Cursor />
         <main>{children}</main>
         <BackToTop />
       </body>
